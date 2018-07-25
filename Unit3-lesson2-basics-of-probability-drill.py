3e986fc4cbebc5b05c5a72a0d844cd17901a955f
evarubin <35392283+evarubin@users.noreply.github.com>
2018-07-24 20:06:28 -0700

    Create Unit3-lesson2-basics-of-probability-drill.py

---
+++ Unit3-lesson2-basics-of-probability-drill.py
@@ -0,0 +1,23 @@
+# 1. Calculate the probability of flipping a balanced coin four times and getting each pattern: HTTH, HHHH and TTHH.
+# .5 * .5 * .5 * .5 = 6.25%
+
+# 2. If a list of people has 24 women and 21 men, then the probability of choosing a man from the list is 21/45.
+# What is the probability of not choosing a man?
+# 24/45 probability of not choosing a man (choosing a woman)
+
+ # 3. The probability that Bernice will travel by plane sometime in the next year is 10%.
+ # The probability of a plane crash at any time is .005%.
+ # What is the probability that Bernice will be in a plane crash sometime in the next year?
+
+ # .00005 * .1 = .000005
+ # .0005% chance she will crash sometime in the next year
+
+ # 4.  A data scientist wants to study the behavior of users on the company website.
+ # Each time a user clicks on a link on the website, there is a 5% chance that the user
+ # will be asked to complete a short survey about their behavior on the website.
+ # The data scientist uses the survey data to conclude that, on average,
+ # users spend 15 minutes surfing the company website before moving on to other things.
+ # What is wrong with this conclusion?
+
+ # Small sample size along with selection bias - those who click on links may be prone
+ # to staying on site longer than those who don't click links.
